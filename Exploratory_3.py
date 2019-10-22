a38fbca3ee783bccbfae539850cd0d18bf541333
P3nr0se <55242595+P3nr0se@users.noreply.github.com>
2019-10-22 10:54:03 -0400

    Update 2
    Added Commands

---
+++ Exploratory_3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+import re
+def magnifying_glass("book"):
+    if book != string:
+        raise TypeError("I don't feel like doing math.")
+
+magnifying_glass("A_Modest_Proposal.txt")
